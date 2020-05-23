a88e2ba9d545aa54505e75749eb43ecfe999af7c
diverdane <dane.leblanc@cyberark.com>
2020-05-22 20:09:42 -0400

    Add Kubernetes-in-Docker based automated tests
    This change adds automated testing that uses Kubernetes-in-Docker (KinD)
    to spin up Kubernetes clusters using various Kubernetes versions.
    
    Currently, the Kubernetes versions being used for testing are:
    - Kubernetes v1.18.2
    - Kubernetes v1.16.9
    - Kubernetes v1.14.10
    - Kubernetes v1.12.10
    
    The tests that are run for each version of Kubernetes include:
    - KinD create of a Kubernetes cluster.
    - Helm install of Conjur/Nginx + backend Postgres database.
    - Helm test of the Conjur deployment that confirms that the
      Conjur status page is active.
    - Helm delete of Conjur release.
    - KinD delete of the Kubernetes cluster.

--- test-minimal.sh
+++ test-minimal.sh
@@ -1,34 +1,15 @@
 #!/bin/bash -e
 
-# This script runs the minimal helm test, without relies on external load balancers
-# or persistent volumes. This is suitable for environment where these resources aren't
-# available.
+# This script runs the minimal helm test, without relies on external load
+# balancers or persistent volumes. This is suitable for environment where
+# these resources aren't available.
+#
+# This test also enables Helm test logging by default.
 
-# Run helm test
-# Any arguments passed to this script will be passed to `helm test`.
+export HELM_INSTALL_ARGS="--set service.external.enabled=false \
+                   --set postgres.persistentVolume.create=false"
+export HELM_INSTALL_TIMEOUT="180s"
+export HELM_TEST_LOGGING=${HELM_TEST_LOGGING:-true}
 
-HELM_TEST_ARGS="${@:---cleanup}"  # cleanup test pod by default
+./test.sh
 
-RELEASE_NAME="helm-chart-test-$(date -u +%Y%m%d-%H%M%S)"
-
-function finish() {
-  echo "> Deleting release $RELEASE_NAME"
-  helm del --purge $RELEASE_NAME
-}
-trap finish EXIT
-
-echo "> Installing helm chart, waiting until app is ready..."
-dataKey="$(docker run --rm cyberark/conjur data-key generate)"
-
-# External load balancers and persistent volumes are not always available
-# so they are turned off.
-helm install --wait \
-             --timeout 180 \
-             --name $RELEASE_NAME \
-             --set "dataKey=$dataKey" \
-             --set "service.external.enabled=false" \
-             --set "postgres.persistentVolume.create=false" \
-             ./conjur-oss
-
-echo "> Running helm tests with arguments: $HELM_TEST_ARGS"
-helm test --logs $HELM_TEST_ARGS $RELEASE_NAME
--- test.sh
+++ test.sh
@@ -1,24 +1,69 @@
 #!/bin/bash -e
 
-# Run helm test
-# Any arguments passed to this script will be passed to `helm test`.
+# Run Helm test
+#
+# This script does the following in sequence:
+# - Runs a Helm install of a Conjur server
+# - Runs a Helm test that deploys a test container that runs a
+#   Bash Automated Test System (a.k.a. "Bats") test script that
+#   confirms that the Conjur server's status page is active.
+#
+# Syntax:
+#       ./test.sh <your-helm-test-args>
+#
+# Optional Environment Variables:
+#   HELM_INSTALL_ARGS:    Additional arguments to pass to the `helm install`
+#                         command beyond the standard arguments used below:
+#                             --wait
+#                             --timeout $HELM_INSTALL_TIMEOUT
+#                             --set "dataKey=$dataKey"
+#                         Defaults to empty string.
+#   HELM_TEST_LOGGING:    Set to true to enable Helm test log collection.
+#                         Defaults to false.
+#   HELM_INSTALL_TIMEOUT: Helm install timeout. Defaults to `900s`.
 
-HELM_TEST_ARGS="${@:---cleanup}"  # cleanup test pod by default
+# Command line arguments for this script are passed to `helm test`.
+HELM_TEST_ARGS="$@"
+HELM_INSTALL_ARGS=${HELM_INSTALL_ARGS:-""}
+HELM_TEST_LOGGING=${HELM_TEST_LOGGING:-false}
+HELM_INSTALL_TIMEOUT=${HELM_INSTALL_TIMEOUT:-900s}
+
+# If Helm test logging is to be enabled, then we want to ensure that the
+# test pod is not deleted until after Helm test has had a chance to display
+# its logs.
+if [ "$HELM_TEST_LOGGING" = "true" ]; then
+  HELM_INSTALL_ARGS="${HELM_INSTALL_ARGS} --set test.deleteOnSuccess=false"
+  HELM_TEST_ARGS="${HELM_TEST_ARGS} --logs"
+fi
 
 RELEASE_NAME="helm-chart-test-$(date -u +%Y%m%d-%H%M%S)"
 
-function finish() {
-  echo "> Deleting release $RELEASE_NAME"
-  helm del --purge $RELEASE_NAME
+function delete_release() {
+  echo "=========================================="
+  echo "Deleting Conjur Helm release $RELEASE_NAME"
+  echo "=========================================="
+  helm del "$RELEASE_NAME"
 }
-trap finish EXIT
 
-echo "> Installing helm chart, waiting until app is ready..."
+echo "======================================================="
+echo "Installing Conjur OSS, waiting until server is ready..."
+echo "======================================================="
 dataKey="$(docker run --rm cyberark/conjur data-key generate)"
 helm install --wait \
-             --timeout 900 \
-             --name $RELEASE_NAME \
-             --set "dataKey=$dataKey" ./conjur-oss
+             --timeout $HELM_INSTALL_TIMEOUT \
+             --set "dataKey=$dataKey" \
+             $HELM_INSTALL_ARGS \
+             "$RELEASE_NAME" \
+             ./conjur-oss
+trap delete_release EXIT
 
-echo "> Running helm tests with arguments: $HELM_TEST_ARGS"
-helm test $HELM_TEST_ARGS $RELEASE_NAME
+echo "=================================================="
+echo "Running helm tests with arguments:"
+echo "    $HELM_TEST_ARGS"
+echo "=================================================="
+helm test $HELM_TEST_ARGS "$RELEASE_NAME"
+
+if  [ "$HELM_TEST_LOGGING" = "true" ]; then
+  # Test pod log has been displayed, so it's safe to delete the test pod.
+  kubectl delete pod -l release="$RELEASE_NAME"
+fi
